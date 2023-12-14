5a337a1feb9400fc975f8df730df3bca46d834fb
Viraj <virajjadhao85@gmail.com>
2023-12-14 21:08:01 +0530

    city and temp pulled

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        WEATHER_API_KEY : 'aa585628c7a99e9b9ba675d1581d775d'
+      },
+}
 
 module.exports = nextConfig
