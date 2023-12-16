c291cdb6ad97992533e2d58d19707d9c74d8729d
Viraj <virajjadhao85@gmail.com>
2023-12-16 18:18:00 +0530

    done with weather app

--- app/page.tsx
+++ app/page.tsx
@@ -28,12 +28,13 @@ export default function Home() {
       })
     setLoading(false)
   }
+  console.log(data)
   if(loading){
     <h1>Loading..............</h1>
   }else{
   return (
     <main>
-      <div className='w-full h- bg-slate-700 flex align items-center flex-col h-96'>
+      <div className='w-full bg-slate-700 flex align items-center flex-col h-screen'>
         <form onSubmit={fetchWeather}>
           <input type="text" value={city} id="search-navbar" onChange={(e) => setCity(e.target.value)} className="w-60 m-7 p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter City Name.." />
           <button onClick={fetchWeather} className="border-gray-300" type='submit'>Submit</button>
--- components/Card.tsx
+++ components/Card.tsx
@@ -5,9 +5,12 @@ const Card = ({ data , city }) => {
     return (
         <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div className="flex flex-col items-center pb-10">
-                <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://img.freepik.com/premium-psd/sunny-rainy-cloudy-day-weather-forecast-icon-illustration_47987-10695.jpg" alt="Bonnie image" />
+                <img className="m-10 w-24 h-24 mb-3 rounded-full shadow-lg" src="https://img.freepik.com/premium-psd/sunny-rainy-cloudy-day-weather-forecast-icon-illustration_47987-10695.jpg" alt="Bonnie image" />
                 <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">City : {city}</h5>
-                <span className="text-sm text-gray-500 dark:text-gray-400">Temperature : {data.main.temp_max}</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Min-Temp : {data.main.temp_min}</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Max_Temp : {data.main.temp_max}</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Weather : {data.weather[0].main}</span>
+                <span className="text-sm text-gray-500 dark:text-gray-400">Description : {data.weather[0].description}</span>
             </div>
         </div>
 
